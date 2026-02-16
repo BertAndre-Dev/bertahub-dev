181d7ff2956721f7d4d06a9fa5684cc3ab9f9812
admin <admin@admins-MacBook-Pro.local>
2026-02-16 14:01:35 +0100

    cookies and poolicy

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono, Varela_Round, Manrope } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 import ClientProvider from "@/components/client-provider/page";
+import { CookieBanner } from "@/components/cookie-banner";
 
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
@@ -28,6 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`font-sans antialiased`}>
         <ClientProvider>{children}</ClientProvider>
+        <CookieBanner />
         <Analytics />
       </body>
     </html>
